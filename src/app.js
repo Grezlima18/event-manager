c396c8a0199cb1f81f6773229523b7656034b56e
GiordanoComin <116606326+GiordanoComin@users.noreply.github.com>
2025-11-19 15:20:54 -0300

    app.js atualizado com rateLimiting middleware

--- src/app.js
+++ src/app.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const rateLimit = require('./middlewares/rateLimiting');
+const ipLogger = require('./middlewares/ipLogger');
 require('dotenv').config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(rateLimit(100, 60000));
+app.use(ipLogger);
 
 app.get('/', (req, res) => {
   res.json({ message: "Event Management API - Online" });
 });
 
 module.exports = app;
+
+//teste
