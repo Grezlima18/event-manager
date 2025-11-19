8fa4a44a8c6d9c0b0f5ac66f4e4c07b0836b8356
GiordanoComin <116606326+GiordanoComin@users.noreply.github.com>
2025-11-19 15:19:51 -0300

    atualizando events.js com o novo middleware

--- src/routes/events.js
+++ src/routes/events.js
@@ -1,24 +1,61 @@
 const express = require('express');
 const router = express.Router();
+
 const EventController = require('../controllers/EventController');
 const { createEventValidation, eventIdParamValidation } = require('../validations/eventValidations');
 const validationHandler = require('../middlewares/validationHandler');
 const authMiddleware = require('../middlewares/auth');
+const rateLimit = require('../middlewares/rateLimiting');
 
-// Rotas públicas
-router.get('/', EventController.listEvents);
-router.get('/:id', eventIdParamValidation, validationHandler, EventController.getEventById);
+//aa
 
-// Rotas protegidas (Create, Update, Delete)
+/* ============================
+   ROTAS PÚBLICAS
+================================ */
+
+// [GET] /events - Lista eventos (público)
+router.get('/', rateLimit(50, 60000), EventController.listEvents);
+
+// [GET] /events/:id - Detalhes de um evento (público)
+router.get(
+    '/:id',
+    eventIdParamValidation,
+    validationHandler,
+    rateLimit(50, 60000),
+    EventController.getEventById
+);
+
+/* ============================
+   ROTAS PROTEGIDAS
+================================ */
+
 router.use(authMiddleware);
 
 // [POST] /events - Criar evento
-router.post('/', createEventValidation, validationHandler, EventController.createEvent);
+router.post(
+    '/',
+    rateLimit(20, 60000),
+    createEventValidation,
+    validationHandler,
+    EventController.createEvent
+);
 
 // [PUT] /events/:id - Atualizar evento
-router.put('/:id', eventIdParamValidation, validationHandler, EventController.updateEvent);
+router.put(
+    '/:id',
+    rateLimit(20, 60000),
+    eventIdParamValidation,
+    validationHandler,
+    EventController.updateEvent
+);
 
 // [DELETE] /events/:id - Deletar evento
-router.delete('/:id', eventIdParamValidation, validationHandler, EventController.deleteEvent);
+router.delete(
+    '/:id',
+    rateLimit(20, 60000),
+    eventIdParamValidation,
+    validationHandler,
+    EventController.deleteEvent
+);
 
 module.exports = router;
